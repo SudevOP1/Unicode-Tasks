e5fae1008a0f74a67d2611b5bcb23ae70c51d2e4
SudevOP1 <sudevdahitule06@gmail.com>
2024-10-04 22:31:36 +0530

    email error

--- Task 3/backend_fitness_app/home/urls.py
+++ Task 3/backend_fitness_app/home/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('api/create-user/', views.create_user, name="create_user"),
+    path('api/user-list/', views.user_list, name="user_list"),
+    path('api/user-details/<str:user_id>/', views.user_details, name="user_details"),
 ]
