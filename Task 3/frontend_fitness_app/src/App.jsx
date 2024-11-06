0064627112c645f7793c366cdab10d46aace1ee7
SudevOP1 <sudevdahitule06@gmail.com>
2024-11-06 21:22:57 +0530

    BrowserRouter Error

--- Task 3/frontend_fitness_app/src/App.jsx
+++ Task 3/frontend_fitness_app/src/App.jsx
@@ -2,6 +2,8 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PrivateRoute from "./utils/PrivateRoute";
 import { AuthProvider } from "./context/AuthContext";
+import { useContext } from 'react';
+import AuthContext from "./context/AuthContext";
 
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
@@ -9,40 +11,40 @@ import HomePage from "./pages/HomePage";
 import ProfilePage from "./pages/ProfilePage";
 import CreateRoutinePage from "./pages/CreateRoutinePage";
 import Header from "./components/Header";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
+  let {user} = useContext(AuthContext);
   return (
     <div>
       <BrowserRouter>
         <AuthProvider>
           <Header />
           <Routes>
-
             <Route
               path="/"
-              element={<PrivateRoute><HomePage /></PrivateRoute>}
+              element={
+                user
+                ? <PrivateRoute><HomePage /></PrivateRoute>
+                : <LandingPage />
+              }
             />
-
-            <Route
-              path="/profile"
-              element={<PrivateRoute><ProfilePage /></PrivateRoute>}
-            />
-
-            <Route
-              path="/create-routine"
-              element={<PrivateRoute><CreateRoutinePage /></PrivateRoute>}
-            />
-
             <Route
               path="/login"
               element={<LoginPage />}
             />
-
             <Route
               path="/register"
               element={<RegisterPage />}
             />
-
+            <Route
+              path="/profile"
+              element={<PrivateRoute><ProfilePage /></PrivateRoute>}
+            />
+            <Route
+              path="/create-routine"
+              element={<PrivateRoute><CreateRoutinePage /></PrivateRoute>}
+            />
           </Routes>
         </AuthProvider>
       </BrowserRouter>
--- Task 3/frontend_fitness_app/src/utils/PrivateRoute.jsx
+++ Task 3/frontend_fitness_app/src/utils/PrivateRoute.jsx
@@ -4,7 +4,6 @@ import { Navigate } from 'react-router-dom';
 import AuthContext from '../context/AuthContext';
 
 const PrivateRoute = ({ children }) => {
-  // const isAuthenticated = false;
   let {user} = useContext(AuthContext);
   return(
     <div>
